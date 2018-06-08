d0ecd5a354ac2f165e468d8cb33a754e1835e2db
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-08 11:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(name){
-  if(katzDeliLine ===[]){
+  if(katzDeliLine.length===0){
     return "There is nobody waiting to be served!"
   }
   else{

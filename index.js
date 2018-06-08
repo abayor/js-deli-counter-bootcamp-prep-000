95c45814ac23b76a001e22f2269da70b5c76d551
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-08 11:51:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,15 +17,6 @@ function nowServing(katzDeliLine){
   }
   
 }
-if (x.length === 0) {
-      return "The line is currently empty."
-    } else {
-      for(var i = 0; i < x.length; i++) {
-        line += (i + 1) + ". " + x[i] + ", "
-      }
-      line = line.slice(0, line.length-2)
-      return "The line is currently: " + line
-    }
 
 
 function currentLine(katzDeliLine){
@@ -36,7 +27,7 @@ function currentLine(katzDeliLine){
     for(var i=0; i < katzDeliLine.length; i++){
       
       var name = katzDeliLine[i];
-      var  sn = i+1 "." + name ",";
+      var  sn = i+1 +"." + name + ",";
       
       return  "The line is currently:"+ sn  ;
     }

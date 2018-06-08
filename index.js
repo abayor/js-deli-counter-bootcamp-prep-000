bb8a9cebc78736fa570094a39b8645007687e6de
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-08 10:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli=[];
 var otherDeli=[];
 
 function takeANumber(newMember,oldMember){
-   katzDeli.push("welcome","Ada");
+   katzDeli.push(`welcome, ${oldMember}. You are number ${katzDeli.index);
 return katzDeli;
 }
 

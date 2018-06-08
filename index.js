4b2574a1cb355925ff9424f513b6bcc4ab5c0d55
Abayomi Akinsola <olasunkami010@gmail.com>
2018-06-08 12:06:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function currentLine(katzDeliLine){
       
     }
      lines = lines.slice(0, lines.length-2)
-     return  "The line is currently:" + " " + lines  ; 
+     return  "The line is currently:" + lines  ; 
      
   }
 
